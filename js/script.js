83f9083ef4827205a70b04718bc10ab0e6e5b694
Kirill <batomunkuevkirill@yandex.ru>
2022-03-19 09:42:47 +0700

    Make burger

--- js/script.js
+++ js/script.js
@@ -11,17 +11,17 @@ function ibg() {
 ibg();
 
 let header__burger = document.querySelector(".header__burger");
-let header_menu = document.querySelector(".header__menu");
-let header = document.querySelector(".header");
-let header__list = document.querySelector(".header__list");
+let header__menu = document.querySelector(".header__menu");
+let header__bottom = document.querySelector(".header-bottom");
+let menu__list = document.querySelector(".menu__list");
 
 header__burger.onclick = function () {
   header__burger.classList.toggle("active");
-  header_menu.classList.toggle("active");
-  header.classList.toggle("active");
+  header__menu.classList.toggle("active");
+  header__bottom.classList.toggle("active");
 };
 
-header__list.onclick = function () {
-  header__list.classList.remove("active");
-  header.classList.remove("active");
+menu__list.onclick = function () {
+  menu__list.classList.remove("active");
+  header__bottom.classList.remove("active");
 };
